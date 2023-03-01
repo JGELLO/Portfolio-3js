7da94568f99c46db42ade9ed39fb2a6f0b39fc00
Angelo Alcera Cabaluna <cabsgelo561561@gmail.com>
2023-03-01 10:32:31 +0800

    new file:   donut.glb 	renamed:    ast.jpg -> images/ast.jpg 	renamed:    donut.jpg -> images/donut.jpg 	renamed:    galaxy.jpg -> images/galaxy.jpg 	renamed:    gel.jpg -> images/gel.jpg 	renamed:    mars.jpg -> images/mars.jpg 	renamed:    metal.jpg -> images/metal.jpg 	renamed:    pepe.jpg -> images/pepe.jpg 	renamed:    rings.jpg -> images/rings.jpg 	renamed:    saturn.jpg -> images/saturn.jpg 	renamed:    space.jpg -> images/space.jpg 	renamed:    stars.jpg -> images/stars.jpg 	renamed:    sun.jpg -> images/sun.jpg 	modified:   index.html 	modified:   main.js 	new file:   startext.gltf

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ import './style.css'
 
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
+// Camera
 			const scene = new THREE.Scene();
-			const camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 0.1, 1000 );
+			const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 			const renderer = new THREE.WebGLRenderer({
         canvas:document.querySelector('#bg'),
@@ -14,15 +16,36 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 			document.body.appendChild( renderer.domElement );
 
 
-      // const marsmaterial = new THREE.TextureLoader().load('mars.jpg');
+// Gltf file
+
+// const loader = new GLTFLoader();
+
+// loader.load( 'donut.glb', function ( gltf ) {
+
+// 	scene.add( gltf.scene );
+
+// }, undefined, function ( error ) {
+
+// 	console.error( error );
+
+// } );
+
+// Avatar
+
+const heatTexture = new THREE.TextureLoader().load('images/sun.jpg');
+
+const heat = new THREE.Mesh(new THREE.SphereGeometry(4, 32, 12 ), new THREE.MeshBasicMaterial({ map: heatTexture }));
+
+scene.add(heat);
 
 		
+// Torus Shape
 
-const torusTexture = new THREE.TextureLoader().load('ast.jpg');
+const torusTexture = new THREE.TextureLoader().load('images/ast.jpg');
 
 const torus = new THREE.Mesh(new THREE.TorusGeometry( 8, 1.7, 94, 50  ), new THREE.MeshBasicMaterial({ map: torusTexture }));
       
-const torus2Texture = new THREE.TextureLoader().load('ast.jpg');
+const torus2Texture = new THREE.TextureLoader().load('images/ast.jpg');
 
 const torus2 = new THREE.Mesh(new THREE.TorusGeometry( 14, 1.7, 94, 50  ), new THREE.MeshBasicMaterial({ map: torus2Texture }));
       
@@ -37,58 +60,15 @@ const torus2 = new THREE.Mesh(new THREE.TorusGeometry( 14, 1.7, 94, 50  ), new T
       camera.position.setZ(30);
       camera.position.setX(-3);
 
-// Avatar
-
-const gelTexture = new THREE.TextureLoader().load('sun.jpg');
-
-const gel = new THREE.Mesh(new THREE.SphereGeometry(4, 32, 12 ), new THREE.MeshBasicMaterial({ map: gelTexture }));
-
-scene.add(gel);
-
-
-
-
-renderer.render(scene, camera);
-
-			function animate() {
-				requestAnimationFrame( animate );
-
-				torus.rotation.x -= 0.03;
-				torus.rotation.y += 0.03;
-
-        torus2.rotation.x -= 0.02;
-			
-
-        gel.rotation.y += 0.03;
-        gel.rotation.x += 0.03;
-
-				renderer.render( scene, camera );
-			}
-
-			animate();
-
-
-    // Background
-
-      const spaceTexture = new THREE.TextureLoader().load('space.jpg');
-        scene.background = spaceTexture;
-    
-    
-// Lights
+      // Lights
 
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
+pointLight.position.set(2, 5, 5);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
-// Helpers
-
-// const lightHelper = new THREE.PointLightHelper(pointLight)
-// const gridHelper = new THREE.GridHelper(200, 50);
-// scene.add(lightHelper, gridHelper)
-
-// const controls = new OrbitControls(camera, renderer.domElement);
+// Stars 
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
@@ -99,9 +79,56 @@ function addStar() {
     .fill()
     .map(() => THREE.MathUtils.randFloatSpread(100));
 
-  star.position.set(x, y, z);
-  scene.add(star);
+      star.position.set(x, y, z);
+      scene.add(star);
 }
 
 Array(200).fill().forEach(addStar);
 
+// Scroll Animation
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  torus.rotation.x += 0.05;
+  torus2.rotation.y += 0.075;
+  torus2.rotation.z += 0.05;
+
+  heat.rotation.y += 0.01;
+  heat.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+
+// Background
+
+const spaceTexture = new THREE.TextureLoader().load('images/space.jpg');
+  scene.background = spaceTexture;
+    
+    
+
+
+
+function animate() {
+  requestAnimationFrame(animate);
+
+				torus.rotation.x -= 0.04;
+				torus.rotation.y += 0.04;
+
+        torus2.rotation.x -= 0.02;
+        torus2.rotation.y += 0.02;
+			
+
+        heat.rotation.y += 0.03;
+        heat.rotation.x += 0.03;
+
+        renderer.render(scene, camera);
+      
+    }
+
+animate();
